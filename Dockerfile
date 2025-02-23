63588a3ee79345acfa255d4fa45510126094619c
Sarah Groß <me@sarahg.de>
2025-02-23 22:27:58 +0100

    moved setup to new base image version, implemented file name base and network share options

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ghcr.io/connum/debian-base-scansupport:7.7.0
+FROM ghcr.io/connum/debian-base-scansupport:7.7.1
 
 LABEL io.hass.version="1.0" io.hass.type="addon" io.hass.arch="aarch64|amd64"
 
@@ -7,41 +7,8 @@ ENV TERM="xterm-256color"
 
 # Setup base
 RUN apt-get update && apt-get install -y --no-install-recommends \
-        git \
-        sane \
-        sane-utils \
-        sed \
-        dbus \
-        scanbd \
-    # sane-scan-pdf dependencies
-        bash \
-        netpbm \
-        ghostscript \
-        poppler-utils \
-        units \
-        imagemagick \
-        unpaper \
-        util-linux \
-        tesseract-ocr \
-        tesseract-ocr-all \
-        parallel \
-        bc \
     # for debugging only
-        nano \
-    && mkdir -p /var/run/dbus/ \
-    # sane-scan-pdf
-    && git clone --branch master https://github.com/rocketraman/sane-scan-pdf.git /opt/sane-scan-pdf && \
-        chmod a+x /opt/sane-scan-pdf/scan /opt/sane-scan-pdf/scan_perpage && \
-        ln -s /opt/sane-scan-pdf/scan /usr/local/bin/sane-scan-pdf \
-    # patch sane-scan-pdf script
-    && sed -i 's|\${XDG_DATA_HOME:-\$HOME/.local/share}|/config|g' /opt/sane-scan-pdf/scan \
-    && sed -i 's|"\$DIR/scan_perpage"|/opt/sane-scan-pdf/scan_perpage|g' /opt/sane-scan-pdf/scan \
-    # add root user to scanner group
-    && usermod -a -G scanner root \
-    # cleanup
-    && apt-get remove --purge -y \
-        git \
-    && apt-get autoremove -y
+        nano
 
 # Copy root filesystem
 COPY rootfs /
--- rootfs/run.sh
+++ rootfs/run.sh
@@ -2,6 +2,8 @@
 
 ulimit -n 1048576
 
+chmod a+x /usr/bin/get_scan_filename
+
 echo "copying defaults and scan_pre from /opt/sane-scan-pdf to /config/sane-scan-pdf if non-existing"
 mkdir -p "/config/sane-scan-pdf"
 if [ ! -f "/config/sane-scan-pdf/defaults" ]; then
@@ -19,19 +21,6 @@ fi
 ln -s /config/sane-scan-pdf/scan_pre /opt/sane-scan-pdf/scan_pre
 
 chmod a+x /config/sane-scan-pdf/defaults /config/sane-scan-pdf/scan_pre
-
-# custom logger to make default scripts work without needing to install logging packages
-# echo '#!/bin/bash
-# TAG=""; while getopts "t:" opt; do 
-#   case $opt in 
-#     t) TAG="$OPTARG" ;; 
-#   esac 
-# done; shift $((OPTIND-1)); 
-# if [ -n "$TAG" ]; then 
-#   echo "[$TAG] $@"; 
-# else 
-#   echo "$@"; 
-# fi' > /usr/bin/logger && chmod +x /usr/bin/logger
 
 # copy custom component if it doesn't already exist
 if [ ! -d "/homeassistant/custom_components/scan_server_integration" ]; then
@@ -67,7 +56,7 @@ echo "Checking for $CONFIG_PATH_SANED"
 if [ ! -f "$CONFIG_PATH_SANED" ]; then
     # update /config/saned.conf
     echo -e "\n# Allow all private network ranges\nlocalhost\n192.168.0.0/16\n10.0.0.0/8\n172.16.0.0/12" | tee -a $SANED_CONF
-    # Otherwise, back up and move the existing config
+    # Otherwise, move the existing config
     mv "$SANED_CONF" "$CONFIG_PATH_SANED"
 
     echo "created default config"
@@ -86,8 +75,9 @@ SCANBD_CONF="/etc/scanbd/scanbd.conf"
 echo "Checking for $CONFIG_PATH_SCANBD"
 # Check if /config/scanbd.conf exists
 if [ ! -f "$CONFIG_PATH_SCANBD" ]; then
-    # Otherwise, back up and move the existing config
+    # Otherwise, move the existing config
     mv "$SCANBD_CONF" "$CONFIG_PATH_SCANBD"
+    sed -i 's/"test\.script"/"scan.script"/g' "$CONFIG_PATH_SCANBD"
 
     echo "created default config"
 else
@@ -98,15 +88,23 @@ fi
 rm -f "$SCANBD_CONF"
 ln -s "$CONFIG_PATH_SCANBD" "$SCANBD_CONF"
 
-
-if [ ! -d "/config/scripts" ]; then
-    echo "creating default scanbd scripts in /config/scripts"
+SCRIPT_PATH="/config/scripts"
+if [ ! -d "$SCRIPT_PATH" ]; then
+    echo "creating default scanbd scripts in $SCRIPT_PATH"
     
     # Move the folder and its contents
     mv /usr/share/scanbd/scripts /config/
 else
-    echo "Using existing scanbd scripts from /config/scripts"
+    echo "Using existing scanbd scripts from $SCRIPT_PATH"
 fi
+
+SCAN_SCRIPT="$SCRIPT_PATH/scan.script"
+SCAN_SCRIPT_SOURCE="src/scripts/scan.script"
+if [ ! -f "$SCAN_SCRIPT" ]; then
+    mv "$SCAN_SCRIPT_SOURCE" "$SCRIPT_PATH"
+fi
+
+chmod a+x "$SCRIPT_PATH/$(basename "$SCAN_SCRIPT")"
 
 ln -sfn /config/scripts /etc/scanbd/scripts
 
@@ -116,22 +114,69 @@ dbus-daemon --system
 echo "Starting inetd..."
 service openbsd-inetd start
 
-#echo "Starting saned..."
-#export SANE_CONFIG_DIR=/etc/sane.d/
-#saned -elD --user=root -d7
+OPTIONS_FILE="/data/options.json"
 
-#echo "Starting scanbd..."
+# Function to process options
+reload_options() {
+    NETSHARE_SERVER=$(jq -r '.netshare_server' $OPTIONS_FILE)
+    NETSHARE_USERNAME=$(jq -r '.netshare_username' $OPTIONS_FILE)
+    NETSHARE_PASSWORD=$(jq -r '.netshare_password' $OPTIONS_FILE)
+    NETSHARE_PATH=$(jq -r '.netshare_path' $OPTIONS_FILE)
+    NETSHARE_PATH="${NETSHARE_PATH##[\\/]}" # remove leading slashes
+
+    # delete previous mount, ignoring if it doesn't exist
+    curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/mounts/scanserver -X DELETE > /dev/null
+
+    if [[ -n "$NETSHARE_SERVER" ]]; then
+        JSON_DATA=$(jq -n \
+            --arg name "scanserver" \
+            --arg usage "share" \
+            --arg type "cifs" \
+            --arg server "$NETSHARE_SERVER" \
+            --arg share "$NETSHARE_PATH" \
+            --arg username "$NETSHARE_USERNAME" \
+            --arg password "$NETSHARE_PASSWORD" \
+            --argjson read_only false \
+            '{
+                name: $name,
+                usage: $usage,
+                type: $type,
+                server: $server,
+                share: $share,
+                username: $username,
+                password: $password,
+                read_only: $read_only
+            }')
+
+        # Send the JSON data in the POST request
+        RESPONSE=$(curl -sSL -H "Authorization: Bearer $SUPERVISOR_TOKEN" \
+            -H "Content-Type: application/json" \
+            -X POST -d "$JSON_DATA" http://supervisor/mounts)
+        RESPONSE_RESULT=$(jq -r ".result" <<< "$RESPONSE")
+        
+        if [[ $RESPONSE_RESULT != 'ok' ]] then
+            curl -X POST \
+                -H "Authorization: Bearer $SUPERVISOR_TOKEN" \
+                -H "Content-Type: application/json" \
+                -d '{
+                    "message": "The network share could not be mounted, please check the host, user, password and path in the add-on options.",
+                    "title": "Scan Server Error: Could not mount network share",
+                    "notification_id": "scan_server_error"
+                }' \
+                http://supervisor/core/api/services/persistent_notification/create
+        else
+            echo "Network share $NETSHARE_SERVER/$NETSHARE_PATH mounted to /share/scanserver"
+        fi
+        
+    fi
+}
+
+# Initial load of options
+reload_options
+
+# Listen for SIGHUP to trigger reloads
+trap reload_options SIGHUP
+
+echo "Starting scanbd..."
 export SANE_CONFIG_DIR=/etc/scanbd/
 scanbd -d2 -f -c /etc/scanbd/scanbd.conf 
-
-#echo "Detected devices:"
-#scanimage -L
-
-#sane-find-scanner | sed -En '
-#/^found possible / {
-#  s/^found possible USB scanner \(vendor=0x[0-9a-f]+ \[([^]]+)\], product=0x[0-9a-f]+ \[([^]]+)\]\) at (.*)/  - vendor: "\1"\n    product: "\2"\n    address: "\3"/p
-#}
-#' | sed '1i scanners:' > /config/devices.yaml
-#cat /config/devices.yaml
-
-tail -f /dev/null
